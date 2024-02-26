065f92f4941a9c3e5d0a0f98b6816c422c33dcc0
Lenovo <9.3razzinayyuri@gmail.com>
2024-02-26 11:52:48 +0700

    menentukan jejari

--- PertemuanPertamaKelasC.cpp
+++ PertemuanPertamaKelasC.cpp
@@ -3,8 +3,19 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
-    cout << "HI";
+    //begin
+    // numeric nJejari
+    // display 'Masukan Jejari= '
+    // accept nJejari
+    // compute nLuas = 3.14 * r * r
+    // display 'nLuasnya= ' = nLuas
+    //end
+
+    double nJejari, nLuas;
+    cout << "Masukan Jejari = ";
+    cin >> nJejari;
+    nLuas = 3.14 * nJejari * nJejari;
+    cout << "Luasnya = " << nLuas << endl;
 
     system("pause");
 }
